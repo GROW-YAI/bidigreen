ccb3e117eac6cfeaa6f98b6515192164bac85934
MsEvee <baidooevelynyaa@gmail.com>
2024-12-01 12:51:53 +0000

    bidigreen

--- src/components/HomePage/Team.jsx
+++ src/components/HomePage/Team.jsx
@@ -9,8 +9,8 @@ const Team = () => {
     <div>
       {/* Header Section */}
       <div className="flex flex-col justify-center items-center mb-6">
-        <h1 className="text-3xl font-bold">OUR TEAM</h1>
-        <h3 className="text-lg text-gray-600">Our Production Team</h3>
+        <h1 className="text-3xl font-bold ">OUR TEAM</h1>
+        <h3 className="text-lg text-green-500">Our Production Team</h3>
       </div>
 
       {/* Swiper Slider */}
@@ -20,23 +20,23 @@ const Team = () => {
           delay: 3000, // Time in milliseconds between automatic transitions
           disableOnInteraction: false, // Keeps autoplay running even after user interaction
         }}
-        spaceBetween={5} // Space between slides
+        spaceBetween={10} // Space between slides
         slidesPerView={3} // Display three slides at a time
         loop={true} // Enable infinite looping
         className="w-full"
       >
         {TeamMembers.map((team, index) => (
           <SwiperSlide key={index}>
-            <div className="relative w-64 mx-auto group">
+            <div className="relative w-96 mx-auto group">
               <img src={team.image} alt="team image" className="w-full" />
               <div
                 className="absolute bottom-0 left-0 w-full bg-white bg-opacity-80 p-2 text-center 
-                           transition-colors duration-300 ease-in-out group-hover:bg-orange-500"
+                           transition-colors duration-300 ease-in-out group-hover:bg-green-500"
               >
                 <h2 className="text-xl font-bold text-gray-800 group-hover:text-white">
                   {team.name}
                 </h2>
-                <p className="text-sm text-orange-500 font-medium group-hover:text-white">
+                <p className="text-sm text-green-500 font-medium group-hover:text-white">
                   {team.title}
                 </p>
               </div>
